8dba23a2bee15bd11a29772caa7a296d1d25cd58
VictorNalu <victorchinalurumogu@gmail.com>
2023-10-16 18:38:05 -0700

    printtt

--- _printf.c
+++ _printf.c
@@ -1,36 +1,25 @@
 #include "main.h"
-
-
 /**
-
  * _printf - Printf function
-
  * @format: format.
-
  * Return: Printed chars.
-
  */
 
 int _printf(const char *format, ... )
 {
 	int count = 0;
 	va_list args;
-
 	if (format = NULL)
 		return (-1);
 
 	va_start(args, format);
-
 	while (*format)
 	{
-		if (*format != '%')
+	if (*format != '%')
 		{
-			write(1, format, 1);
+			 write(1, format, 1);
 		}
-
 		format++;
 	}
-
 	return (count);
-
 }
